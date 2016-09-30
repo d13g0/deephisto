dccdeff00cac8c118e0525a8daf98959d030dd19
d13g0 <diego.cantor@gmail.com>
2016-09-29 20:32:05 -0400

    work on the topologically aware loss layer

--- deephisto/caffe/LossLayer.py
+++ deephisto/caffe/LossLayer.py
@@ -1,75 +1,72 @@
 import pdb
+import itertools
 import numpy as np
 import matplotlib.pylab as plt
 import caffe
+import math
 
 from caffe._caffe import layer_type_list
 
 
 class TopoLossLayer(caffe.Layer):
-
     def setup(self, bottom, top):
-        if len(bottom) !=2:
+        if len(bottom) != 2:
             raise Exception('We need two inputs to compute loss')
         self.counter = 0
 
     def reshape(self, bottom, top):
-
-        self.height = bottom[0].height
-        self.width = bottom[0].width
         self.diff = np.zeros_like(bottom[0].data, dtype=np.float32)
         top[0].reshape(1)
 
     def forward(self, bottom, top):
 
-        label = bottom[1].data[0,...]
-        label = label.transpose(1,2,0)
-        label = label[:,:,0]
+        pred = np.argmax(bottom[0].data[0, ...], axis=0)
+        label = bottom[1].data[0, ...]
+        label = label.transpose(1, 2, 0)
+        label = label[:, :, 0]
 
-        pred = bottom[0].data[0,...]
-        pred = pred.argmax(axis=0)
-
-        scores = bottom[0].data
-        scores -= np.max(scores)
+        inputs = bottom[0].data[0, ...]
+        scores = inputs - np.max(inputs, axis=0)
         exp_scores = np.exp(scores)
-        probs = exp_scores / np.sum(exp_scores,axis=1, keepdims=True)
-        probs = probs[0,...]  #remove batch dim
 
-        max_prob = np.argmax(probs, axis=0)
+        if np.all(np.sum(exp_scores, axis=0, keepdims=True)) == 0:
+            print 'PROBLEM'
+            pdb.set_trace()
 
+        probs = exp_scores / np.sum(exp_scores, axis=0, keepdims=True)
+
+        if not np.amax(probs) <= 1 or not np.amin(probs) >= 0:
+            print 'PROBLEM'
+            pdb.set_trace()
 
         expected_value = np.zeros_like(pred, dtype=np.float32)
-        for i in range(0,10):
-            k = i+1 #shift
+        for i in range(0, len(probs)):
+            k = i
             expected_value += probs[i] * k
 
-        expected_value -= 1 #shifting back
+        diff = np.exp(label) - np.exp(expected_value)
 
-
-        diff  = np.abs(np.exp(label)-np.exp(pred))
-        diff2 = np.abs(np.exp(label)-np.exp(expected_value))
-
+        loss = 0
+        _, h, w = probs.shape
+        for i in range(h):
+            for j in range(w):
+                idx = int(label[i, j])
+                loss += log_prob[idx, i, j]  # loss only sum the log_probs corresponding to the true classes
 
         self.counter += 1
 
-        if self.counter == 10:
-
+        if self.counter == 100:
             self._visualize(label, pred, diff)
-            self._show_channels(bottom[0].data[0,...], title='input channels')
-
-            self._show_channels(probs, title='probablity maps after soft-max')
-            self._show('max_prob', max_prob)
-            self._show('expected', expected_value)
-            self._show('loss with expected value (now differentiable)',diff2, use_label_range=False)
-
+            titles = ['1. input', '2. scores', '3. exp_scores', '4. probability', '5.log prob']
+            steps = [inputs, scores, exp_scores, probs, log_prob]
+            self._show_channels(steps, titles)
             plt.show()
-            pdb.set_trace()
+            self.counter = 0
 
-        #self.diff = diff
-        self.loss = np.sum(diff2**2)
-
+        self.probs = probs
+        self.loss = loss
+        self.label = label
         top[0].data[...] = self.loss
-
 
     def backward(self, top, propagate_down, bottom):
         """
@@ -78,34 +75,28 @@ class TopoLossLayer(caffe.Layer):
         """
         assert propagate_down[1] != True, 'gradients cannot be calculated with respect to the label inputs'
 
+        loss = self.loss
+        grad = self.probs
+        label = self.label
+
+        _, h, w = grad.shape
+        for i in range(h):
+            for j in range(w):
+                idx = int(label[i, j])
+                grad[idx, i, j] = (grad[idx, i, j] - 1) / loss
+
+        # assert np.any(np.max(np.abs(self.grad))) <=  math.exp(10), 'numeric problem'
+
+        print 'loss %.2f  min: %f max: %f' % (self.loss, grad.min(), grad.max())
+
         if propagate_down[0]:
-
-            bottom_diff = bottom[0].diff[0,...]
-
-            print 'loss %.2f count: %d, min: %.2f max: %.2f'%(self.loss, self.counter, bottom_diff.min(), bottom_diff.max())
-
-            label = bottom[1].data[0, ...]
-            label = label.transpose(1, 2, 0)
-            label = label[:, :, 0]
-
-            pred = bottom[0].data[0, ...]
-            pred = pred.argmax(axis=0)
-
-            for i in range(0, self.height):
-                for j in range(0, self.width):
-                    bottom_diff[int(label[i,j]),i,j] = self.diff[i,j]
-
-
-            if self.counter == 10:
-                self._show_channels(bottom_diff, title='backward diff')
-                plt.show()
-                self.counter = 0
-
-
+            bottom[0].diff[0, ...] = grad
 
     def _visualize(self, label, pred, diff):
 
         fig, ax = plt.subplots(1, 3, squeeze=True)
+
+        fig.canvas.set_window_title('Prediction')
 
         for i in range(0, 3):
             ax[i].get_xaxis().set_visible(False)
@@ -125,7 +116,7 @@ class TopoLossLayer(caffe.Layer):
 
         plt.tight_layout()
 
-    def _show(self, title, image, use_label_range = True):
+    def _show(self, title, image, use_label_range=True):
         fig = plt.figure()
         fig.canvas.set_window_title(title)
         if use_label_range:
@@ -135,20 +126,26 @@ class TopoLossLayer(caffe.Layer):
         ax = plt.gca()
         ax.format_coord = self._get_formatter(title, image)
 
-    def _show_channels(self, channels, title=None):
-        N = len(channels)
+    def _show_channels(self, steps, titles):
 
-        fig, ax = plt.subplots(1, N, figsize=(16, 3))
-        fig.canvas.set_window_title('Channels %s'%title)
-        for i in range(N):
-            ax[i].set_title('%d' % i)
-            ax[i].imshow(channels[i], interpolation='none', vmin = channels.min(), vmax= channels.max(), cmap='gist_heat')
-            ax[i].get_xaxis().set_visible(False)
-            ax[i].get_yaxis().set_visible(False)
-            ax[i].format_coord = self._get_formatter('Channel %d' % i, channels[i])
+        N = len(steps)
+        C = len(steps[0])
+
+        fig, ax = plt.subplots(ncols=C, nrows=N, squeeze=False, figsize=(16, 8), facecolor='black')
+
+        fig.canvas.set_window_title('Loss Layer Processing')
+
+        for i, step in enumerate(steps):
+            ax[i, 0].set_ylabel(titles[i], rotation=90, size='large', color='#cccccc')
+            for j in range(C):
+                ax[i, j].set_axis_bgcolor('black')
+                ax[i, j].imshow(step[j], interpolation='none', vmin=step[j].min(), vmax=step[j].max(), cmap='hot')
+                ax[i, j].get_xaxis().set_visible(False)
+                ax[i, j].get_yaxis().set_ticks([])
+                ax[i, j].format_coord = self._get_formatter('%s - Channel [%d]' % (titles[i], j), step[j])
 
         plt.tight_layout()
-
+        fig.subplots_adjust(top=0.95, bottom=0.05, wspace=0.0, hspace=0.0)
 
     def _get_formatter(self, title, img):
 
