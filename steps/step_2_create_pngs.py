b613250058d22341b8421d76204d501458eba871
d13g0 <diego.cantor@gmail.com>
2017-01-05 21:03:34 -0500

    improved documentation

--- steps/step_2_create_pngs.py
+++ steps/step_2_create_pngs.py
@@ -13,9 +13,8 @@ from config import dh_load_subjects, dh_read_config
 
 def dh_get_histo_range(config):
     """
-    This function allows to check the mapping between the histology range and the label range.
-    The histology domain values should provide a good estimate for the
-    HISTOLOGY_RANGE_MIN and HISTOLOGY_RANGE_MAX under [histology] in the configuration file
+    The reported histology value range should provide a good estimate for the
+    HISTOLOGY_RANGE_MIN and HISTOLOGY_RANGE_MAX that yoou can set under [histology] in the configuration file
     """
     utils = ImageUtils(config)
     subjects = dh_load_subjects(config)
@@ -30,22 +29,21 @@ def dh_get_histo_range(config):
         print '%s\t\t[%.2f,%.2f]\t\t     [%d,%d]'%(s,min,max,min_l,max_l)
     print
 
+
 def main():
     """
+    utils.create_png_images:
+
     Creates PNGs for all the subjects in the subject list. The NIFTI images from where these PNGs are generated
     must have been downloaded first (step 1).
 
-    Uncoment the code below to create the PNG images. The evaluation mode allows
-    reviewing the actual histology (max, min) values and the corresponding linear mapping to
-    label values with the current NUM_LABELS parameter without generating the actual PNGs
+    Make sure that you set NUM_LABELS, HISTOLOGY_RANGE_MIN and HISTOLOGY_RANGE_MAX in the configuration file before
+    creating running this step.
 
-    The HISTOLOGY_RANGE_MIN and HISTOLOGY_RANGE_MAX parameters determine the extent used (domain) to
-    map histology values to labels (range).
+    The HISTOLOGY_RANGE_MIN and HISTOLOGY_RANGE_MAX parameters determine how histology values are mapped to labels.
     These two parameters correspond to cmin, and cmax in scipy.misc.bytescale
     See: https://docs.scipy.org/doc/scipy-0.18.1/reference/generated/scipy.misc.bytescale.html
 
-    Make sure that you set NUM_LABELS, HISTOLOGY_RANGE_MIN and HISTOLOGY_RANGE_MAX before
-    creating the PNG files and moving to the next step.
     """
 
     config = dh_read_config('/home/dcantor/projects/deephisto/code/config_neuronal_density.ini')
@@ -56,9 +54,11 @@ def main():
 
     dh_get_histo_range(config)
 
+
+    # comment this for loop if you only want to run dh_get_histo_range. This will report the domain values (histology)
     for s in subjects:
         print s
-        utils.create_png_images(s) # Uncomment to generate PNGs
+        utils.create_png_images(s)
 
 if __name__ == '__main__':
     main()
